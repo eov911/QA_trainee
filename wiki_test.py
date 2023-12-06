7c1aa7c800c5180695a4af2a08e2020d2c756d9d
Оксана Ерегина <oksanaeregina@gmail.com>
2023-12-06 17:49:36 +0300

    Хранение информации из таблицы реализовано с помощью dataclasses

--- wiki_test.py
+++ wiki_test.py
@@ -1,19 +1,32 @@
 import requests
+from dataclasses import dataclass, field
 from bs4 import BeautifulSoup
+
+
+@dataclass
+class Website:
+    webs: list = field(default_factory=list)
+
+
+web = Website()
 url = 'https://en.wikipedia.org/wiki/Programming_languages_used_in_most_popular_websites'
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find('table')
+page = BeautifulSoup(response.text, 'lxml')
+quotes = page.find('table')
 trs = quotes.find_all('tr')
-A = []
 for tr in trs:
     B = []
     tds = tr.find_all('td')
     for td in tds:
-        B.append(td.text.rstrip())
+        string = td.text.rstrip()
+        while '[' in string:
+            quote1 = string.find('[')
+            quote2 = string.find(']')
+            string = string[:quote1] + string[quote2+1:]
+        B.append(string)
     if len(B) != 0:
-        B[1] = B[1].replace(',','')
-        B[1] = B[1].replace('.','')
+        B[1] = B[1].replace(',', '')
+        B[1] = B[1].replace('.', '')
         x = ''
         for el in B[1]:
             if el in '0123456789':
@@ -21,16 +34,14 @@ for tr in trs:
             else:
                 break
         B[1] = x
-        A.append(B)
-# for el in A:
-#     print(el)
-#     print('*******************************')
-
-param = 10**10
-key = True
-for website in A:
-    if int(website[1]) < param:
-        print('LOL')
-        key = False
-if key == True:
-    print('It\'s OK')
+        web.webs.append(B)
+parametrs = [10**7, 1.5 * 10**7, 5 * 10**7, 10**8, 5 * 10**8,
+             10**9, 1.5 * 10**9]
+for param in parametrs:
+    for website in web.webs:
+        if int(website[1]) < param:
+            print(f'{website[0]} (Frontend:{website[2]}|Backend:', end='')
+            print(f'{website[3]}) has {website[1]} unique visitors', end=' ')
+            print(f'per month. (Expected more than {int(param)})')
+            print()
+            key = False
